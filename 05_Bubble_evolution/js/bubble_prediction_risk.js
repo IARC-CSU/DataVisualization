b5818f37900fba6f4dd82635d349e4d0af49fcad
Unknown <timat35@gmail.com>
2018-12-06 09:09:00 +0100

    add download button

--- 05_Bubble_evolution/js/bubble_prediction_risk.js
+++ 05_Bubble_evolution/js/bubble_prediction_risk.js
@@ -1,3 +1,27 @@
+function writeDownloadLink(){
+    var html = d3.select("svg")
+        .attr("title", "svg_title")
+        .attr("version", 1.1)
+        .attr("xmlns", "http://www.w3.org/2000/svg")
+        .node().parentNode.innerHTML;
+    
+    d3.select(this)
+        .attr("href-lang", "image/svg+xml")
+        .attr("href", "data:image/svg+xml;base64,\n" + btoa(html))
+        .on("mousedown", function(){
+            if(event.button != 2){
+                d3.select(this)
+                    .attr("href", null)
+                    .html("Use right click");
+            }
+        })
+        .on("mouseout", function(){
+            d3.select(this)
+                .html("Download");
+        });
+};
+
+
  function bubble_evo() // generate heatmap 
 	{ 	
 		var file_use = "data/prediction_risk.csv"; 
