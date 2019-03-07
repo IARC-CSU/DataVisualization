04edf9a78425b83d7514e5e59f3fd03d6b1b9faa
LAM Frédéric <lamf@iarc.fr>
2019-03-07 16:46:52 +0100

    Colors GCO

---
+++ 00_GCO_conf/gco-colors.js
@@ -0,0 +1,161 @@
+var CanColors = {
+
+	'bootstrap_colors' : [ '#044362' , '#0A70A1' , '#9B7000' , '#FFB903' , '#9B2000' , '#C62900' , '#9B5300' , '#FF8A03'] , 
+
+	'continents_tree' : { 
+
+		1 : '#004529' ,  4 : '#7A0177' , 2 : '#FF0000' , 3 : '#E31A1C' , 5: '#33a7ff' , 6 : '#FFD92F' , // basics
+		966 : '#004529' ,  967 : '#7A0177' , 999 : '#ff0f0f' , 968 : '#08306B' , 969 : '#FFD92F',  // double for globocan , 
+		962 : '#FFA500' , 905 : '#fc2121'
+	} , 
+
+	'continents' : {
+		966 : '#004529' ,  
+		967 : '#7A0177' , 
+		999 : '#ff0f0f' , 
+		968 : '#08306B' , 
+		969 : '#FFD92F',  
+		962 : '#FFA500' , 
+		905 : '#fc2121'
+	} , 
+
+	// hdi colors 
+	'hdi_regions' : {
+		'981' : '#0b9aba' , 
+		'982' : '#5c2d4f' , 
+		'983' : '#d40f50' , 
+		'984' : '#fadb14'
+	}, 
+	'hdi_regions_index' : {
+		'1' : '#0b9aba' , 
+		'2' : '#5c2d4f' , 
+		'3' : '#d40f50' , 
+		'4' : '#fadb14'
+	}, 
+	'hdi_regions_code' : {
+		'VHHD' : '#0b9aba' , 
+		'HHD' : '#5c2d4f' , 
+		'MHD' : '#d40f50' , 
+		'LHD' : '#fadb14'
+	}, 
+	'hubs' : {
+		'AFCRN' : '#71C8E3' , 
+		'CARIB' : '#b21c01' , 
+		'IZMIR' : '#724a98' , 
+		'LA' : '#e0c61f',
+		'MUMB' : '#2eaf81',
+		'PI' : '#ff6600'
+	}, 
+
+	'INCOME' : {
+		'LOW' : '#bf226e' , 
+		'HIGH' : '#0068e8' , 
+		'UPPER_MIDDLE' : '#cacc64' , 
+		'LOWER_MIDDLE' : '#659c57',
+	}, 
+
+	// low-less
+	'more_less_dev_regions' : {
+		'901' : '#57313b' , 
+		'902' : '#bdad80'
+	}, 
+
+	// who regions 
+	'who_regions' : {
+		'991' : '#d9343f' , 
+		'992' : '#e6b31c' ,
+		'993' : '#0d890f' , 
+		'994' : '#14a9e7' , 
+		'995' : '#21d998' , 
+		'996' : '#4500b5' , 
+	} , 
+
+	// who regions 
+	'who_regions_per_code' : {
+		'AFRO' : '#d9343f' , 
+		'PAHO' : '#e6b31c' ,
+		'EMRO' : '#0d890f' , 
+		'EURO' : '#14a9e7' , 
+		'SEARO': '#21d998' , 
+		'WPRO' : '#4500b5' , 
+	} , 
+
+	// regions colors 
+	'regions': {
+		'1' : '#238544' , // Eastern Africa
+		'2' : '#257A58' , // Middle Africa
+		'3' : '#0F603F' , // Northern Africa
+		'4' : '#002C1A' , // Southern Africa
+		'5' : '#05492d' , // Western Africa
+
+		// ASIA 
+		'10' : '#460043' , // Eastern Asia
+		'11' : '#63005F' , // South Eastern Asia
+		'12' : '#970A91' , // South central Asia
+		'13' : '#A12C9C' , // Western Asia 
+
+		// EUROPE
+		'14': '#34a9c7' , // Central & Eastern Europe
+		'15': '#296e91' , // Northern Europe
+		'16': '#485796' , // Southern Europe
+		'17': '#7197e3' ,  // Western Europe
+
+		// AMERICA 
+		'9': '#fc2121' , // Northern America
+		'6': '#b80000' , // Caribbean
+		'7': '#fc3d3d' , // Central America
+		'8': '#d90000' ,  // South America
+
+		// OCEANIA
+		'18': '#B09000' , // Australia/New Zealand
+		'19': '#E3BB05' , // Melanesia
+		'20': '#FFE055' , // Micronesia
+		'21': '#FFE87F' , // Polynesia
+		'22': '#FFE87F' , // Other oceanian regions
+	}, 
+	'regions_globocan' : {
+
+		// AFRICA 
+		'910' : '#238544' , // Eastern Africa
+		'911' : '#257A58' , // Middle Africa
+		'912' : '#0F603F' , // Northern Africa
+		'913' : '#002C1A' , // Southern Africa
+		'914' : '#05492d' , // Western Africa
+
+		// ASIA 
+		'906' : '#460043' , // Eastern Asia
+		'920' : '#63005F' , // South Eastern Asia
+		'921' : '#970A91' , // South central Asia
+		'922' : '#A12C9C' , // Western Asia 
+
+		// EUROPE
+		'923': '#34a9c7' , // Central & Eastern Europe
+		'924': '#296e91' , // Northern Europe
+		'925': '#485796' , // Southern Europe
+		'926': '#7197e3' ,  // Western Europe
+
+		// AMERICA 
+		'905': '#fc2121' , // Northern America
+		'915': '#b80000' , // Caribbean
+		'916': '#fc3d3d' , // Central America
+		'931': '#d90000' ,  // South America
+
+		// OCEANIA
+		'927': '#B09000' , // Australia/New Zealand
+		'928': '#E3BB05' , // Melanesia
+		'954': '#FFE055' , // Micronesia
+		'957': '#FFE87F' , // Polynesia
+	}, 
+
+	// continents : to delete
+	// 'continents' : [ '#a9d4ba' ,  '#f0c878' , '#6cbbe0' , '#e65757' , '#2c7d91' , '#c29b4e' , '#f03b20' , '#ffeda0' , '#dd1c77' , '#e0ecf4' , '#8856a7' , '#bdbdbd' ] , 
+	
+	// sexes
+	'sexes' : [  '#1E90FF' , '#5e0099' , '#ff7070' ] , 
+	
+	// 	
+	'type' : { 'incidence' : null , 'mortality' : null } , 
+
+	'indicator' : { 0 : '#6cbbe6' , 1 : '#e65a53' , 2 : '#006d2c' } , 
+	
+}
